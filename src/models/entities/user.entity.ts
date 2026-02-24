5c1ece976d8a96d99efe2a48151df98326bf651e
Viktor-Adam Koropecky <46943555+vikroe@users.noreply.github.com>
2026-02-24 15:55:37 +0100

    user entity

---
+++ src/models/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryKey, Property, Unique } from "@mikro-orm/core";
+
+@Entity({ tableName: "users" })
+@Unique({ properties: ["email"] })
+export class User {
+    @PrimaryKey()
+    id!: number;
+
+    @Property({ length: 64 })
+    email!: string;
+
+    @Property({ length: 256 })
+    passwordHash!: string;
+
+    @Property({ onCreate: () => new Date() })
+    createdAt: Date = new Date();
+
+    @Property({ onCreate: () => new Date(), onUpdate: () => new Date() })
+    updatedAt: Date = new Date();
+}
